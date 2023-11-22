fbc300e91b2e67e03b9e3a3336dece3752a8e2af
PabloBnv <bnvpablo@gmail.com>
2023-11-21 21:35:03 -0300

    Update Header.jsx

--- pelisvhx/src/components/Header.jsx
+++ pelisvhx/src/components/Header.jsx
@@ -3,40 +3,41 @@ import './header.css'
 function Header() {
 
     return (
-        <><header className="headerclass">
-            <div className='logodiv'>
-                <img src="vhxlogo.gif" className='logo'></img>
-                <h1>| Pelis Vhx |</h1>
-            </div>
-            
-            <div className='divdrop'>
-                <div className="dropdown">
-                    <button class="dropbtn">Dropdown</button>
-                    <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
+        <>
+            <header className="headerclass">
+                <div className='logodiv'>
+                    <img src="vhxlogo.gif" className='logo'></img>
+                    <h1>| Pelis Vhx |</h1>
                 </div>
-                <div className="dropdown">
-                    <button class="dropbtn">Dropdown</button>
-                    <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
-                </div>
-                <div className="dropdown">
-                    <button class="dropbtn">Dropdown</button>
-                    <div class="dropdown-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
-                </div>
-            </div>
 
-        </header>
+                <div className='divdrop'>
+                    <div className="dropdown">
+                        <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                    <div className="dropdown">
+                        <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+                </div>
+
+            </header>
 
 
         </>
